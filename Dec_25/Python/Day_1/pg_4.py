c39d57d07539ad81cf5827138d95f5f8e55fb668
ffdrko <fahim.deepto@northsouth.edu>
2025-12-14 21:45:24 +0600

    Day-1 multiple input

---
+++ Dec_25/Python/Day_1/pg_4.py
@@ -0,0 +1,11 @@
+user_message = "Enter a todo: "
+user_todo_1 = input(user_message)
+user_todo_2 = input(user_message)
+user_todo_3 = input(user_message)
+user_todo_4 = input(user_message)
+
+
+print(user_todo_1)
+print(user_todo_2)
+print(user_todo_3)
+print(user_todo_4)
