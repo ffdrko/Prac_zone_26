e96b38fe8a75a7e0af897973813ab599d4f0e21c
ffdrko <fahim.deepto@northsouth.edu>
2025-12-14 21:46:31 +0600

    Day-1 list intro.

--- Dec_25/Python/Day_1/pg_4.py
+++ Dec_25/Python/Day_1/pg_4.py
@@ -4,8 +4,5 @@ user_todo_2 = input(user_message)
 user_todo_3 = input(user_message)
 user_todo_4 = input(user_message)
 
-
-print(user_todo_1)
-print(user_todo_2)
-print(user_todo_3)
-print(user_todo_4)
+user_todos = [user_todo_1, user_todo_2, user_todo_3, user_todo_4]
+print(user_todos)
