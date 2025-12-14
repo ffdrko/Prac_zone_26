d0003f7694826116e1678e7aef4976e9f8e29f3f
ffdrko <fahim.deepto@northsouth.edu>
2025-12-14 22:36:16 +0600

    Day-1 code experiment

---
+++ Dec_25/Python/Day_1/Code_Ex/cx_1.py
@@ -0,0 +1,8 @@
+user_prompt = "Enter a todo: "
+todo_1 = input(user_prompt)
+todo_2 = input(user_prompt)
+todo_3 = input(user_prompt)
+
+todos = [todo_1, todo_2, todo_3, "Hello"]
+print(todos)
+print(type(todos))
