a769331f9127cef905666b516e70beef1a8c689e
ffdrko <fahim.deepto@northsouth.edu>
2025-12-14 21:53:33 +0600

    Day-1 list test

--- Dec_25/Python/Day_1/pg_5.py
+++ Dec_25/Python/Day_1/pg_5.py
@@ -2,4 +2,4 @@ first_name = "Fahim"
 middle_name = "Faisal"
 last_name = "Deepto"
 
-person_name = [first_name, middle_name, last_name]
+person_name = [first_name, middle_name, last_name, "New York"]
