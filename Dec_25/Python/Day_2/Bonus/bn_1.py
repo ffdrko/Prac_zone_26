6ec43c356c7195a659fe5f13f456fbeafee9d243
ffdrko <fahim.deepto@northsouth.edu>
2025-12-16 14:05:50 +0600

    Day-2 code bonus password checker.

---
+++ Dec_25/Python/Day_2/Bonus/bn_1.py
@@ -0,0 +1,8 @@
+user_prompt = input("Please enter a password: ")
+
+while user_prompt != "pass123":
+    print("Please enter a password again")
+    user_prompt = input("Please enter a password: ")
+
+
+print("Welcome User!")
